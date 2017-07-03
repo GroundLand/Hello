a84b1d87b62ff2dbcb56732c12bdee743b373096
chenlianfac <chenlianfac@163.com>
2017-07-03 17:06:32 +0800

    二维数组测试

---
+++ src/main/java/Enum/EnumTest.java
@@ -0,0 +1,10 @@
+package Enum;
+
+/**
+ * Created by Administrator on 2017/7/3 0003.
+ */
+ class EnumTest {
+     public static void main(String args[]){
+         System.out.println(AccountType.FIXED);
+     }
+}
--- src/main/java/singleTest/test1.java
+++ src/main/java/singleTest/test1.java
@@ -5,17 +5,10 @@ package singleTest;
  */
 public class test1 {
     public static void main(String [] args){
-        Integer a = Integer.valueOf(11);
-        Integer b = new Integer(11);
-        int c = 11;
-        System.out.println(a.getClass());
-        System.out.println(a==c);
-        System.out.println(b==c);
 
-        String str1 = "aaaa";
-        String str2 = "aa";
-        String str3 = "aa";
-        System.out.println((str2+str3));
+
+
+        Operator.main(new String [] {"12"});
 
     }
 }
