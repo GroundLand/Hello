3f0166d174e15d2e416676423cbffe2f20dd2d15
GroundLand <1311961744@qq.com>
2017-10-29 21:57:07 +0800

    枚举

--- src/main/java/importJAVASE/Enum/AccountType.java
+++ src/main/java/importJAVASE/Enum/AccountType.java
@@ -6,8 +6,20 @@ import org.omg.CORBA.PRIVATE_MEMBER;
  * Created by Administrator on 2017/7/3 0003.
  */
 enum AccountType {
-    SAVINT,FIXED,CURRENT;
-    private AccountType(){
+    SAVINT(1,"存在"),FIXED(2,"充满"),CURRENT(3,"当前的");
+    //构造器默认私有
+    AccountType(int num,String desr){
         System.out.println("It is a private method");
+    }
+    private int num;
+
+    public int getNum() {
+        return num;
+    }
+
+    private String desr;
+
+    public String getDesr() {
+        return desr;
     }
 }
