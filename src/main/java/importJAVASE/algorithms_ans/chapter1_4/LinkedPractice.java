93cec6ba1edfe741d5a4d91c168b42758a336d0b
chenlian <chenlian@yuantutech.com>
2018-01-19 16:55:10 +0800

    "the practice of linked"

--- src/main/java/importJAVASE/algorithms_ans/chapter1_4/LinkedPractice.java
+++ src/main/java/importJAVASE/algorithms_ans/chapter1_4/LinkedPractice.java
@@ -4,23 +4,24 @@ import com.google.common.base.Strings;
 import edu.princeton.cs.algs4.StdOut;
 import importJAVASE.algorithms_ans.Linked;
 
+import java.util.LinkedList;
+
 /**
  * Created by evel on 2017/12/25.
  */
 public class LinkedPractice {
 
 
-
-
     public static void main(String[] args) {
-        Linked<Integer> first = new Linked<>();
-        first.add(1);
-        first.add(2);
-        first.add(4);
-        first.add(5);
-        StdOut.println("max()---最大值为："+first.max());
-        Linked.max(first.getFirst());
+        DoubleNode<Integer> doubleNode = new DoubleNode<>();
+        doubleNode.addLast(1);
+        doubleNode.addFirst(2);
+//        doubleNode.addFirst(3);
+//        doubleNode.addFirst(4);
+      //  doubleNode.deHead();
 
-
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.addLast(1);
+        linkedList.addFirst(2);
     }
 }
