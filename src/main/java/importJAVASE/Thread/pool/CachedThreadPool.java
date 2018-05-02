fa8f52405c0a282af30a0beaf2333a6032784f01
chenlian <chenlian@yuantutech.com>
2018-05-02 18:07:11 +0800

    ExecutorService

--- src/main/java/importJAVASE/Thread/pool/CachedThreadPool.java
+++ src/main/java/importJAVASE/Thread/pool/CachedThreadPool.java
@@ -2,17 +2,20 @@ package importJAVASE.Thread.pool;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
+/**
+ * ExcutorServie : newFixedThreadPool(int poolSize),
+ *                 newSingleThreadExecutor(),
+ *                 newCachedThreadPool()
+ */
 public class CachedThreadPool {
-    public static void main(String[] args) {
-        ExecutorService exec = Executors.newCachedThreadPool();
-        LiftOff liftOff=new LiftOff();
-        Future future=exec.submit(liftOff);
-        System.out.println(exec.isShutdown());
-
+    public static void main(String[] args) throws Exception {
+        ExecutorService exec = Executors.newSingleThreadExecutor();
+       // Future future = null;
+        for (int i = 0; i < 5; i++)
+             exec.submit(new LiftOff());
         exec.shutdown();
-        future.cancel(false);System.out.println(exec.isShutdown());
+        System.out.println(exec.isShutdown());
 
     }
 }
