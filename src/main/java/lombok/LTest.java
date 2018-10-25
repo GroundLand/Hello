6c681f0ba2dbb84d29070b6f23be5fd02c045148
cln <cln@servyou.com.cn>
2018-10-25 15:03:19 +0800

    add logback configure

--- src/main/java/lombok/LTest.java
+++ src/main/java/lombok/LTest.java
@@ -1,21 +1,25 @@
 package lombok;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  *
  * @author cln
  * @version $Id: LTest.java v 0.1 18-10-23 上午10:40 cln Exp $$
  */
+@Slf4j
 public class LTest {
 
     public static void main(String args[]) {
         //可以用val
         val paper = new Paper("yellow");   //使用构造函数
 
-        System.out.println("颜色为: " + paper.getColor());
+        log.info("颜色为: {}", paper.getColor());
 
         //使用setter方法
         paper.setColor("blue");
 
-        System.out.println("使用其toString方法： " + paper);
+        log.info("使用其toString方法： {}", paper);
+
     }
 }
