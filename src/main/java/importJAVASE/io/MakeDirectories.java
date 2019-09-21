1db91ed6fb07dc128e7fe64fc126354120951e83
cln <cln@servyou.com.cn>
2019-09-21 17:42:19 +0800

    delete useless files

--- src/main/java/importJAVASE/io/MakeDirectories.java
+++ src/main/java/importJAVASE/io/MakeDirectories.java
@@ -1,10 +1,14 @@
 package importJAVASE.io;
 
+
 /**
  * Created by Administrator on 2017/6/5 0005.
  */
 public class MakeDirectories {
-    private static void usage() {
-        System.out.println();
+
+    public static void main(String[] args) {
+        System.out.println(System.getProperties().toString().replace(", ","\n"));  // 获取工作空间路径
+
     }
+
 }
